6ad1efd294c54fcbf3633033556fbe9df92e6b7f
Oscarwasoccupied <z_xw99@outlook.com>
2025-02-24 18:12:08 -0800

    Add unit test for AViT MoE model with parameterized configurations

--- bubbleformer/models/tests/test_get_model.py
+++ bubbleformer/models/tests/test_get_model.py
@@ -63,3 +63,28 @@ def test_unet_modern(time_window, fields, hidden_channels):
     y = model(x)
 
     assert y.shape == (2, time_window, fields, *spatial_dims)
+
+@pytest.mark.parametrize("fields", [1, 2])
+@pytest.mark.parametrize("patch_size", [4, 8, 16])
+@pytest.mark.parametrize("embed_dim", [192, 384])
+def test_avit_moe(fields, patch_size, embed_dim):
+    """
+    Test AViT model with random configs
+    """
+    model_params = {
+        "fields": fields,
+        "patch_size": patch_size,
+        "embed_dim": embed_dim,
+        "num_heads": 4,
+        "processor_blocks": 4,
+        "drop_path": 0.1,
+        "n_experts": 6,
+        "n_shared_experts":1,
+        "top_k":2,
+    }
+    spatial_dims = 64, 64
+    model = get_model("avit_moe", **model_params)
+    x = torch.randn(2, 3, fields, *spatial_dims) # (B, T, C, H, W)
+    y = model(x) # (B, T, C, H, W)
+
+    assert y.shape == (2, 3, fields, *spatial_dims)
