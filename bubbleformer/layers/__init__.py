d389cbe4878a513d6436b29e6976adaeb784c7f3
Oscarwasoccupied <z_xw99@outlook.com>
2025-03-15 01:55:03 -0700

    Add MoETracker to layer imports for enhanced tracking capabilities

--- bubbleformer/layers/__init__.py
+++ bubbleformer/layers/__init__.py
@@ -1,6 +1,6 @@
 from .positional_encoding import ContinuousPositionBias1D, RelativePositionBias
 from .linear_layers import GeluMLP, SirenMLP
 from .patching import HMLPEmbed, HMLPDebed
-from .moe import MoE
+from .moe import MoE, MoETracker
 from .attention import AxialAttentionBlock, AxialAttentionMoEBlock, TemporalAttentionBlock
 from .conv_layers import ClassicUnetBlock, ResidualBlock, MiddleBlock
